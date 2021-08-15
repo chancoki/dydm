3f961b4b6e15688c8b025b46165d730a24d886f9
chancoki <51854166+chancoki@users.noreply.github.com>
2021-08-15 13:52:02 +0800

    1

---
+++ main.js
@@ -0,0 +1,100 @@
+window.onload = function () {
+  const list = document.querySelector(".list");
+  var room = new danmaku('290935', {
+    debug: false, //存储到indexedDB
+  });
+  //系统事件
+  room.on("connect", function () {
+    console.log("[connect] roomId=%s", this.roomId);
+  });
+  room.on("disconnect", function () {
+    console.log("[disconnect] roomId=%s", this.roomId);
+  });
+  room.on("error", function (err) {
+    console.log("[error] roomId=%s", this.roomId);
+  });
+  //消息事件
+  room.on("chatmsg", function (res) {
+    const duoyu = document.querySelectorAll(".duoyu");
+    if (window.screen.availHeight * 2 < duoyu.length * 45) {
+      for (
+        let i = Number.parseInt(window.screen.availHeight / 45);
+        duoyu.length > i;
+        i--
+      ) {
+        list.removeChild(duoyu[i]);
+      }
+    }
+
+    // const div = document.createElement("div");
+    // div.className = "duoyu";
+    list.innerHTML += `
+    <div class="duoyu">
+    <div class="level" style="background:${
+      res.level <= 5
+        ? "#21B8FC"
+        : res.level <= 10
+        ? "#25D8E6"
+        : res.level <= 15
+        ? "#FDAA29"
+        : res.level <= 20
+        ? "#FD6E21"
+        : res.level <= 25
+        ? "#EC1A20"
+        : res.level <= 30
+        ? "#BE29E6"
+        : ''
+    }">
+       lv.${res.level < 10 ? "0" + res.level : res.level}
+    </div>
+    <div class="user">
+    ${res.nn}：
+    </div>
+    <div class="text" style="color:${
+      res.level <= 5
+        ? "#FFFFFF"
+        : res.level <= 8
+        ? "#2887EA"
+        : res.level <= 11
+        ? "#7CC752"
+        : res.level <= 14
+        ? "#FD6CB4"
+        : res.level <= 17
+        ? "#FD7F24"
+        : res.level <= 20
+        ? "#9A44F0"
+        : res.level <= 30
+        ? "#FC0D1C"
+        : ''
+    }">
+    ${res.txt}
+    </div>
+    </div>
+
+    `;
+    // list.appendChild(div);
+  });
+  room.on("loginres", function (res) {
+    console.log("[loginres]", "登录成功");
+  });
+  room.on("uenter", function (res) {
+    list.innerHTML += `
+    <div class="duoyu" style='margin: 20px 0'>
+    <div class="danm">
+      进入
+    </div>
+    <div class="user">
+    ${res.nn}
+    </div>
+    `;
+    console.log("[uenter]", `${res.nn}进入房间`);
+  });
+  //开始监听
+  room.run();
+
+  //导出日志(不传入房间号默认为所有)
+  room.logger.export(房间号);
+
+  //清除日志(不传入房间号默认为所有)
+  room.logger.clear(房间号);
+};
